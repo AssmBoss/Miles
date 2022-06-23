2a3164828e7f9cdaf3388d675a0b68c23596800a
home <home@MSK-NB-POZ2.ad.kdl-test.ru>
2022-06-23 15:47:54 +0300

    Убраны комментарии, убран System.exit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        // переменные для входных данных и параметров программы:
-        int ticketCost = 600; //для хранения стоимости билета
-        int oneBonusMileCost = 20; //для хранения количества рублей для одной бонусной мили
 
-        if (ticketCost < 0) // проверка, что стоимость билета не отриц.число
-        {
+        int ticketCost = 40;
+        int oneBonusMileCost = 20;
+        boolean isOk = true;
+
+        if (ticketCost < 0) {
             System.out.println("Ошибка: введена отрицательная стоимость билета");
-            System.exit(0); // Terminates JVM
+            isOk = false;
         }
-        if (oneBonusMileCost <= 0) //проверка, что кол-во рублей для одной бонусной мили не 0 и не отрицательное число
-        {
+        if (oneBonusMileCost <= 0) {
             System.out.println("Ошибка: введена отрицательное или нулевое кол-во рублей для одной бонусной мили");
-            System.exit(0); // Terminates JVM
+            isOk = false;
         }
-        int bonusMiles = ticketCost / oneBonusMileCost; // Рассчитывается количество бонусных миль, ответ сохраняется в новую переменную и
-        System.out.println("Вам полагается бонусных миль: " + bonusMiles); // Ответ выводится на экран
+        if (isOk) {
+            int bonusMiles = ticketCost / oneBonusMileCost;
+            System.out.println("Вам полагается бонусных миль: " + bonusMiles);
+        }
     }
 }
